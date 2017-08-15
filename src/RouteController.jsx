448d6b2855bbd9d934108001c7b59faa7d05372d
xianshannan <xianshannan@qq.com>
2017-08-15 14:25:18 +0800

    fix BrowserRouter在页面重载时，引起的多一次渲染问题

--- src/RouteController.jsx
+++ src/RouteController.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
-import { getParams, pathnameAdapter } from './util';
+import { Route } from 'react-router-dom';
+import { getParams } from './util';
 import { ControllerConfig } from './Controller';
 import DefaultNotMatchComponent from './DefaultNotMatchComponent';
 
@@ -111,26 +111,30 @@ class RouteController extends React.Component {
       //这里也必须赋值
       this.pathname = pathname;
       return true;
-    }
-    //切换页面时，首先会触发一次componentWillReceiveProps，不需要重渲染
-    //然后componentWillReceiveProps相应的触发setConfig
-    //setConfig里面触发了setState，改变了state中config的值，这个时候才需要渲染
-    //pathname要相等才渲染，因为this.pathname在下面被重新赋值了
-    if (this.pathname === pathname) {
+    } else if (this.pathname === pathname) {
+      //切换页面时，首先会触发一次componentWillReceiveProps，不需要重渲染
+      //然后componentWillReceiveProps相应的触发setConfig
+      //setConfig里面触发了setState，改变了state中config的值，这个时候才需要渲染
+      //pathname要相等才渲染，因为this.pathname在下面被重新赋值了
       return true;
+    } else {
+      //页面切换componentWillReceiveProps运行过后，这里也会运行一次，但是不需要重新渲染，
+      //然后componentWillReceiveProps里运行的setConfig触发state变化后，
+      //这个函数还会运行一遍，然后返回true，render函数就渲染出切换的页面。
+      //这里this.pathname赋了新值，跟当前页面的pathname相等了，是否渲染切换后的页面就根据this.pathname来判断了
+      this.pathname = pathname;
+      return false;
     }
-    //页面切换componentWillReceiveProps运行过后，这里也会运行一次，但是不需要重新渲染，
-    //然后componentWillReceiveProps里运行的setConfig触发state变化后，
-    //这个函数还会运行一遍，然后返回true，render函数就渲染出切换的页面。
-    //这里this.pathname赋了新值，跟当前页面的pathname相等了，是否渲染切换后的页面就根据this.pathname来判断了
-    this.pathname = pathname;
-    return false;
   }
   componentDidMount() {
     this.setConfig();
   }
   componentWillReceiveProps(nextProps) {
     var pathname = this.getPathNameByHistory();
+    if (this.pathname === undefined) {
+      //兼容react-router中页面重载，在safari中这里会运行多一次的问题
+      return;
+    }
     const { hot } = nextProps;
     //是否开启热替换，这里做了热替换处理兼容处理
     if (hot) {
@@ -178,13 +182,6 @@ class RouteController extends React.Component {
           config &&
           !config.LayoutComponent &&
           <Route path={config.path} component={config.component} />}
-        {false &&
-          pathname === '/' &&
-          ControllerConfig.indexPath &&
-          <Redirect
-            from="/"
-            to={pathnameAdapter(ControllerConfig.indexPath)}
-          />}
       </span>
     );
   }
